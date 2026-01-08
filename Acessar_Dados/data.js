56b27f2c0a7b2e5b0bbfb6a52e2b6b80939f7a55
Thiago Domingos Venturim Ribeiro dos Santos <thiagodomingosventutim@gmail.com>
2026-01-07 21:26:39 -0300

    Json aninhado em data frame

---
+++ Acessar_Dados/data.js
@@ -0,0 +1,17 @@
+data = [
+    {
+        "Emp":"Jeff Russell",
+        "POs":[
+                {"Pono":2608,"Total": 35},
+                {"Pono":2617,"Total": 35},
+                {"Pono":2620,"Total": 139}
+            ]
+    },
+    {  
+        "Emp" : "Jane Boorman",
+        "POs":[
+            {"Pono":2621,"Total": 95},
+            {"Pono":2626,"Total": 218}
+        ]
+    }
+    ]
